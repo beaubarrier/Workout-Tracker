bfd0a235a51e11a674f8b7599a9a61da0b61a349
Beau Barrier <beaubarrier@gmail.com>
2021-08-09 09:55:28 -0700

    Updated heroku settings for server

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,18 +3,18 @@ var path = require("path");
 
 
 
-router.get('public/index.html', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"))
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"))
 })
 
 
 router.get('/stats', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/stats.html"))
+    res.sendFile(path.join(__dirname, "public/stats.html"))
 })
 
 
 router.get('/exercise', function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    res.sendFile(path.join(__dirname, "public/exercise.html"))
 })
 
 module.exports = router;
